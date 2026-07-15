731a1a3feb46d482afd2a2524f9104d7f27c5da8
Abdullahi Hashim Bala <ahashimballer@gmail.com>
2026-07-15 15:11:12 +0100

    imported middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import clerkWebhooks from "./controllers/clerkWebHooks.js";
+import { clerkMiddleware } from "@clerk/express"
 
 connectDB()
 
@@ -12,6 +13,7 @@ app.use(cors()) // Enable Cross-Origin Resource Sharing
 
 // Middleware
 app.use(express.json())
+app.use(clerkMiddleware())
 
 // API to listen to Clerk webhooks
 app.use('/api/clerk', clerkWebhooks);
