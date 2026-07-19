04fc42dc798dabb29d92586a56aec861d1ae4603
Abdullahi Hashim Bala <ahashimballer@gmail.com>
2026-07-19 13:54:27 +0100

    changed room

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -5,7 +5,7 @@ import { createRoom, getOwnerRooms, getRooms, toggleRoomAvailability } from "../
 
 const roomRouter = express.Router();
 
-roomRouter.post('/', protect, upload.array("images", 4), createRoom)
+roomRouter.post('/', upload.array("images", 4), protect, createRoom)
 roomRouter.get('/', getRooms)
 roomRouter.get('/owner', protect, getOwnerRooms)
 roomRouter.post('/toggle-availability', protect, toggleRoomAvailability)
